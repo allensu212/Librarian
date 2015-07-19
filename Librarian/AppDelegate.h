456caa1e8a0c51631b586591dc3e3a7a1a2633d1
Wei-Lun Su <wsu8@illinois.edu>
2015-07-18 23:10:28 -0400

    Initial Commit

---
+++ Librarian/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Librarian
+//
+//  Created by 蘇偉綸 on 2015/7/18.
+//  Copyright (c) 2015年 allensu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
