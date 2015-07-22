e1644250fe4c22d5bc5d927a63c2d5eaef0e450b
Wei-Lun Su <wsu8@illinois.edu>
2015-07-22 12:56:05 -0400

    commit

--- Librarian/BookTableViewCell.m
+++ Librarian/BookTableViewCell.m
@@ -7,18 +7,20 @@
 //
 
 #import "BookTableViewCell.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface BookTableViewCell ()
 
 @property (weak, nonatomic) IBOutlet UILabel *bookTitleLabel;
 @property (weak, nonatomic) IBOutlet UILabel *authorLabel;
+@property (weak, nonatomic) IBOutlet UIView *dotView;
 
 @end
 
 @implementation BookTableViewCell
 
 -(void)configureCellWithDict:(NSDictionary *)dict{
-    
+    self.dotView.layer.cornerRadius = self.dotView.frame.size.width / 2;
     self.bookTitleLabel.text = dict[@"title"];
     self.authorLabel.text = dict[@"author"];
 }
